5870a2480e7d513697fb1274cb29ee2a194128a4
EmirParadi <emir.alejandro.paradi@gmail.com>
2023-11-16 02:18:36 -0300

    fix Ejercicio3 corregido, para pedir los datos por promt

--- js/tp4-ejercicio3.js
+++ js/tp4-ejercicio3.js
@@ -15,42 +15,56 @@ class Rectangulo {
   }
 
   areaRectangulo() {
-    const areaRectangulo = this.alto * this.ancho;
-    document.write(`<h2>El area del rectangulo de base ${this.ancho} por altura ${this.alto} es: <h3>"${areaRectangulo}"</h3><hr>`);
+    if (isNaN(this.alto) || isNaN(this.alto)) {
+      alert("Ingresa valores numericos");
+    } else {
+      const areaRectangulo = this.alto * this.ancho;
+      document.write(`<h2>El AREA del rectangulo de base ${this.ancho} por altura ${this.alto} es: <h3>"${areaRectangulo}"</h3><hr>`);
+    }
   }
 
   perimetroRectangulo() {
-    const perimetroRectangulo = this.alto * 2 + this.ancho * 2;
-    document.write(`<h2>La suma de los lados de su rectangulo da un perimetro de:</h2> <h3>"${perimetroRectangulo}"</h3> <hr>`);
+    if (isNaN(this.alto) || isNaN(this.alto)) {
+      alert("Ingresa valores numericos");
+    } else {
+      const perimetroRectangulo = this.alto * 2 + this.ancho * 2;
+      document.write(`<h2>La suma de los lados de su rectangulo da un PERIMETRO de:</h2> <h3>"${perimetroRectangulo}"</h3> <hr>`);
+    }
   }
 
   mostrarPropiedades() {
-    document.write(`<h2>Su rectangulo tiene una base de ${this.ancho} y tiene una altura de ${this.alto} <br>`);
-    document.write(`
+    if (isNaN(this.alto) || isNaN(this.alto)) {
+      document.write("<h3>Ingreso valores incorrectos");
+    } else {
+      document.write(`<h2>Su rectangulo tiene una base de ${this.ancho} y tiene una altura de ${this.alto} <br>`);
+      document.write(`
     <table>
     <tbody>
     `);
-    for (let indiceAltura = 1; indiceAltura <= this.alto; indiceAltura++) {
-      document.write(`<tr>`);
-      for (let indiceAncho = 1; indiceAncho <= this.ancho; indiceAncho++) {
-        document.write(`
+      for (let indiceAltura = 1; indiceAltura <= this.alto; indiceAltura++) {
+        document.write(`<tr>`);
+        for (let indiceAncho = 1; indiceAncho <= this.ancho; indiceAncho++) {
+          document.write(`
         <td>-<td>
         `);
+        }
+        document.write(`</tr>`);
       }
-      document.write(`</tr>`);
-    }
-    document.write(`</tbody
+      document.write(`</tbody
     </table>`);
+    }
   }
 }
-
 //Creando rectangulos:
 
-const rectangulo1 = new Rectangulo(5, 7);
-
+const rectangulo1 = new Rectangulo(parseInt(prompt(`Ingrese el ancho de su rectangulo`)), parseInt(prompt(`Ingrese el alto de su rectangulo`)));
+prompt;
+rectangulo1.mostrarPropiedades();
 rectangulo1.areaRectangulo();
+rectangulo1.perimetroRectangulo();
+
+document.write(`<hr> <h4>Con valores modificados <h4> <hr>`);
+//modifico los valores
 rectangulo1.modificarPropiedades(2, 3);
 rectangulo1.areaRectangulo();
 rectangulo1.perimetroRectangulo();
-rectangulo1.mostrarPropiedades();
-
