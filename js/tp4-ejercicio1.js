2652ac741a967baf657bb577317d36458170f551
EmirParadi <emir.alejandro.paradi@gmail.com>
2023-11-15 22:58:09 -0300

    Ejericio 1: terminado

---
+++ js/tp4-ejercicio1.js
@@ -0,0 +1,29 @@
+/* 1- Crea un objeto llamado auto que tenga algunas características como el color,
+marca, modelo y si está encendido o apagado. Crea los métodos necesarios para
+permitir encender y apagar el auto.
+Output:
+objeto.encender();
+objeto.apadar()
+auto encendido
+El auto se apagó */
+
+const auto = {
+  //propiedades
+  color: "gris",
+  marca: "Fiat",
+  modelo: "2024",
+  linea: "Sedan",
+  motor: "V6",
+
+  //métodos
+  encender: function () {
+    document.write(`<h4>Auto encendido</h4>`);
+  },
+  apagar: function () {
+    document.write(`<h3>El auto se apagó</h3>`);
+  },
+};
+
+auto.encender();
+document.write("<hr>");
+auto.apagar();
